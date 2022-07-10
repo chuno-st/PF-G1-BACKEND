ed8b30cc7093416359c40d08312b98267ecacf98
chuno-st <49595407+chuno-st@users.noreply.github.com>
2022-07-10 15:31:19 -0300

    variante filtro por categoria

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,42 +1,42 @@
-const { Product, Category } = require ('../db'); 
+const { Product, Category } = require('../db');
 
-const getById = async (req,res) =>{
+const getById = async (req, res) => {
 
     try {
-        const {id} = req.params
+        const { id } = req.params
         const producto = await Product.findByPk(id)
         res.json(producto)
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-const createProduct = (req,res) =>{
+const createProduct = (req, res) => {
     try {
         res.send("Funciona Post")
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-const getProductsOrder = async (req,res)=>{
-    const {order , orderBy} = req.body
+const getProductsOrder = async (req, res) => {
+    const { order, orderBy } = req.body
 
     try {
         switch (orderBy) {
             case "price":
-                if(order === "ASC" || order === "DESC"){
+                if (order === "ASC" || order === "DESC") {
                     const productsOrderByPrice = await Product.findAll({
-                       order : [[orderBy, order]]
+                        order: [[orderBy, order]]
                     })
                     res.json(productsOrderByPrice)
                 }
                 break;
-                
+
             case "name":
-                if(order === "ASC" || order === "DESC"){
+                if (order === "ASC" || order === "DESC") {
                     const productsOrderByABC = await Product.findAll({
-                       order : [[orderBy, order]]
+                        order: [[orderBy, order]]
                     })
                     res.json(productsOrderByABC)
                 }
@@ -46,25 +46,36 @@ const getProductsOrder = async (req,res)=>{
                 const products = await Product.findAll()
                 res.json(products)
                 break;
-            }
-        } 
-            catch (error) 
-            {
-            return res.status(500).json({message: error.message})
-            }
+        }
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 }
 
-const getByCategory = (req,res) =>{
+const getByCategory = async (req, res) => {
     try {
-        const {categori} = req.body
-        const productsByCategory = Product.findAll({
+        const { category } = req.body
+
+        const productsByCategory = await Product.findAll({
             include: {
                 model: Category,
                 required: true
-              }
-        })
+            }
+        });
+        // const productsByCategory = await Product.findAll({
+        //     include: [
+        //         {
+        //             model: Category,
+        //             attributes: ['name'],
+        //             through: {
+        //                 attributes: []
+        //             }
+        //         }
+        //     ]
+        // });
     } catch (error) {
-
+        return res.status(500).json({ message: error.message })
     }
 }
 module.exports = {
