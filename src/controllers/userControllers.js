373d34a8096c36ddb27d826b01a334b76ded26ff
unknown <Lautaro Ferreyra>
2022-07-19 12:25:20 -0300

    rompiendo cosas

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,13 +1,12 @@
 const { User } = require('../db')
 
 const addUser = async (req, res)=>{
-    const {id, name, email} = req.body
-    console.log(`id ${id}, name ${name}, email ${email}`)
+    const {id, userName,email} = req.body
     try {
         const newUser = await User.findOrCreate({
             where:{
                 id,
-                userName: name,
+                userName,
                 email
             }})
         res.json(newUser)
@@ -17,6 +16,25 @@ const addUser = async (req, res)=>{
     }
 }
 
+const checkRole = async (req, res)=>{
+    const {id} = req.query
+    try {
+        if (id) {
+            const newUser = await User.findByPk({
+                id,
+                atributtes
+            })
+            } 
+            if(newUser){
+                res.send(true)
+            }else {
+                res.send(false)
+            }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
 module.exports = {
-    addUser
+    addUser,
+    checkRole
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-const { addUser } = require('../controllers/userControllers')
+const { addUser, checkRole } = require('../controllers/userControllers')
 
 const router = Router();
 
 router.post('/', addUser);
-
+router.get('/checkrole', checkRole)
 
 module.exports = router;
