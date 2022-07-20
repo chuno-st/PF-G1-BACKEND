371d807e49c1718e92c46fb9685e9687cb147f56
unknown <Lautaro Ferreyra>
2022-07-20 12:40:31 -0300

    push standar backend

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const puerto = 3001;
+const PORT = process.env.PORT || 9000;
 const server = require('./src/app.js'); //traemos "server = express"
 const { conn } = require('./src/db.js');
 
 
 //Alter true funciona para al matar terminal y volver a subir se resetea toda la base de datos agregando aquellas columnas que no existen. Luego comentar el alter para que el server levante ya que los ENUM dicen error.
 conn.sync({/*alter: false force: true*/}).then(() => {
-server.listen(puerto, () => {
-    console.log('%s listening at ' + puerto); // Ponemos a escuchar el servidor y consologeamos
+server.listen(PORT, () => {
+    console.log('%s listening at ' + PORT); // Ponemos a escuchar el servidor y consologeamos
   });
 });
