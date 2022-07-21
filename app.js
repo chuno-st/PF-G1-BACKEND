e88453a47307d97797f5695600034555f117b257
unknown <Lautaro Ferreyra>
2022-07-21 09:07:56 -0300

    sacamos url railway

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const PORT = process.env.PORT || 9000;
 const server = require('./src/app.js'); //traemos "server = express"
 const { conn } = require('./src/db.js');
-const URL = process.env.RAILWAY_STATIC_URL
 
 //Alter true funciona para al matar terminal y volver a subir se resetea toda la base de datos agregando aquellas columnas que no existen. Luego comentar el alter para que el server levante ya que los ENUM dicen error.
 conn.sync({/*alter: false force: true*/}).then(() => {
 server.listen(PORT, () => {
-    console.log('%s listening at ' + PORT + ' STATIC URL= ' + URL); // Ponemos a escuchar el servidor y consologeamos
+    console.log('%s listening at ' + PORT ); // Ponemos a escuchar el servidor y consologeamos
   });
 });
