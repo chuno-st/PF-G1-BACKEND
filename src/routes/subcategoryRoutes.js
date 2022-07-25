34d8969c1c0bda12f0844dd2c7d71a3bfd6cd6fc
unknown <Lautaro Ferreyra>
2022-07-25 09:21:12 -0300

    estandarizando develop

--- src/routes/subcategoryRoutes.js
+++ src/routes/subcategoryRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { getSubCategories, deleteSubCategory, updateSubCategory, createSubCategory } = require('../controllers/subcategoryControllers');
-const { checkJwt, checkPermissions } = require('../middlewares/checkAuth0');
+
 
 const router = Router();
 
