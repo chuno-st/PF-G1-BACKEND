9fe1aee63fa04058879c33e5f2f7e2ffbb9879e2
unknown <Lautaro Ferreyra>
2022-07-31 16:20:34 -0300

    cerca

--- src/controllers/webHooksControllers.js
+++ src/controllers/webHooksControllers.js
@@ -1,9 +1,11 @@
 //const { saveSale } = require('./salesControllers');
-//const { purchaseEmail }= require('../mailer/mailer');
+const { purchaseEmail }= require('../mailer/mailer');
 
 const payProduct = async (req,res) => {
     console.log("aca empieza",req.body, "aca termina")
     console.log("req solo", req)
+    const email = "ferreyralautaro69@gmail.com"
+    purchaseEmail(email)
      res.status(200).send("OK")
 }
 
